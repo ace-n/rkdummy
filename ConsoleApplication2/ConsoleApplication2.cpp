403d21ee6c402ee2c75d31c321512f5361f17c6b
Ace Nassri <nassri2@illinois.edu>
2015-05-08 08:09:35 -0500

    Initial

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,55 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <psapi.h>
+
+void PrintProcessNameAndID(DWORD processID)
+{
+	TCHAR szProcessName[512];
+
+	// Get a handle to the process.
+	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
+
+	// Get the process name.
+	if (NULL != hProcess) {
+		HMODULE hMod;
+		DWORD cbNeeded;
+
+		if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &cbNeeded)) {
+			GetModuleBaseName(hProcess, hMod, szProcessName, sizeof(szProcessName) / sizeof(TCHAR));
+		}
+	}
+
+	// Print the process name and identifier.
+	if ((int)processID == 1564) {
+		MessageBoxW(NULL, L"DETECTED!", szProcessName, 0);
+	}
+
+	// Release the handle to the process.
+	CloseHandle(hProcess);
+}
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	while (1) {
+
+		// Get PID list
+		DWORD aProcesses[1024], cbNeeded, cProcesses;
+		unsigned int i;
+		if (!K32EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded)) { return 1; }
+
+		// Get # of PIDs
+		cProcesses = cbNeeded / sizeof(DWORD);
+
+		// Print name/PID for each process.
+		for (i = 0; i < cProcesses; i++) {
+			if (aProcesses[i] != 0) { PrintProcessNameAndID(aProcesses[i]); }
+		}
+
+		Sleep(1000);
+	}
+}
+
